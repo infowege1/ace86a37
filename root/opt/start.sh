83bb51815ec9cbe742963188b170e7120f7de3fd
sybdata <as15@wt.de>
2019-02-12 20:12:58 +0100

    Update start.sh

--- root/opt/start.sh
+++ root/opt/start.sh
@@ -2,3 +2,8 @@
 
 python3 /opt/tv/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
 
+while true; do
+        sleep 6000
+        find /tmp/acehttp.log -depth -type f -mmin +5 -print0 | xargs -0 -r rm -f > /dev/null 2>&1
+done
+
