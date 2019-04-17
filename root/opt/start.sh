62f2351770c83ae89cb0dc6230c64b413368c9b0
sybdata <as15@wt.de>
2019-04-17 21:50:10 +0200

    Update start.sh

--- root/opt/start.sh
+++ root/opt/start.sh
@@ -4,7 +4,6 @@ python3 /opt/tv/HTTPAceProxy-master/acehttp.py >/dev/null 2>&1 &
 
 while true; do
         sleep 21600
-        rm -rf /opt/acelist/playlists/*.m3u > /dev/null 2>&1
         cd /opt/acelist/ && php as.php /opt/acelist/playlists/as > /dev/null 2>&1
 done
 
